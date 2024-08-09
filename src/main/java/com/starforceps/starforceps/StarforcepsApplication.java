cd3440649223572567badb6bfaf58a2d8b36b459
Yongjin An <yongjin5352@gmail.com>
2024-08-09 21:37:40 +0900

    init project

---
+++ src/main/java/com/starforceps/starforceps/StarforcepsApplication.java
@@ -0,0 +1,13 @@
+package com.starforceps.starforceps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarforcepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarforcepsApplication.class, args);
+	}
+
+}
