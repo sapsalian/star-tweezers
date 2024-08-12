e717b6a8fae2e4d256a036ec95b36d181694f63f
Yongjin An <yongjin5352@gmail.com>
2024-08-12 11:10:05 +0900

    feat: OpenAI api를 사용하기 위해 보내는 모든 요청에 openAi 키를 넣어 보낼 수 있도록 설정클래스 정의

---
+++ src/main/java/com/starforceps/starforceps/global/openfeign/OpenAIFeignConfig.java
@@ -0,0 +1,24 @@
+package com.starforceps.starforceps.global.openfeign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAIFeignConfig {
+
+    @Value("${openai-service.api-key}")
+    private String openAiKey;
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) { // 여기에 실제 Bearer 토큰을 설정합니다.
+                requestTemplate.header("Authorization", "Bearer " + openAiKey);
+            }
+        };
+    }
+}
