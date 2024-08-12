7e9b0515bdf5116011d24702e7307082c0308e72
Yongjin An <yongjin5352@gmail.com>
2024-08-12 13:21:04 +0900

    feat: 정리 요청을 받아 내부의 음성파일을 텍스트로 변환하는 서비스 메서드를 테스트하기 위한 controller 구현

---
+++ src/main/java/com/starforceps/starforceps/domain/organizer/api/OrganizerController.java
@@ -0,0 +1,23 @@
+package com.starforceps.starforceps.domain.organizer.api;
+
+import com.starforceps.starforceps.domain.organizer.application.OrganizerService;
+import com.starforceps.starforceps.domain.organizer.dto.OrganizerRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrganizerController {
+    private final OrganizerService organizerService;
+
+    public OrganizerController(OrganizerService organizerService) {
+        this.organizerService = organizerService;
+    }
+
+    @PostMapping(value = "/api/organizer", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String organize(@ModelAttribute OrganizerRequestDto organizerRequestDto) {
+        return organizerService.organize(organizerRequestDto);
+    }
+}
