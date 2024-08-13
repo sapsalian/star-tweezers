b1fae44175816454602ed8882171605d55ec3054
Yongjin An <yongjin5352@gmail.com>
2024-08-13 09:34:39 +0900

    refactor: openai에게 요청해 받은 결과를 정제하는 로직을 OpenAiProvider라는 클래스로 분리

--- src/main/java/com/starforceps/starforceps/domain/organizer/application/OrganizerService.java
+++ src/main/java/com/starforceps/starforceps/domain/organizer/application/OrganizerService.java
@@ -1,79 +1,30 @@
 package com.starforceps.starforceps.domain.organizer.application;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.starforceps.starforceps.domain.organizer.dao.OrganizerRepository;
 import com.starforceps.starforceps.domain.organizer.dto.OrganizerRequestDto;
-import com.starforceps.starforceps.global.openfeign.dto.*;
-import com.starforceps.starforceps.global.openfeign.feign_client.OpenAiClient;
-import io.swagger.v3.core.util.Json;
+import com.starforceps.starforceps.global.openfeign.util.OpenAiProvider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 @Service
 public class OrganizerService {
     private final OrganizerRepository organizerRepository;
-    private final OpenAiClient openAiClient;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final OpenAiProvider openAiProvider;
 
     @Value("${openai-service.assistants.organize}")
     private String organizeAssistants;
 
-    @Value("${openai-service.assistants.exam}")
-    private String examAssistants;
-
-    public OrganizerService(OrganizerRepository organizerRepository, OpenAiClient openAiClient) {
+    public OrganizerService(OrganizerRepository organizerRepository, OpenAiProvider openAiProvider) {
         this.organizerRepository = organizerRepository;
-        this.openAiClient = openAiClient;
+        this.openAiProvider = openAiProvider;
     }
 
     public String organize(OrganizerRequestDto organizerRequestDto) {
-        String lectureContent = audioToText(organizerRequestDto.lectureAudioFile());
+        String lectureContent = openAiProvider.audioToText(organizerRequestDto.lectureAudioFile());
 
-        String lectureSummary = askToGpt(organizeAssistants, lectureContent);
+        String lectureSummary = openAiProvider.askToGpt(organizeAssistants, lectureContent);
 
         return lectureSummary;
     }
 
-    private String audioToText(MultipartFile audioFile) {
-        WhisperResponseDto whisperResponseDto = openAiClient.createTranscription(audioFile, "whisper-1");
-
-        return whisperResponseDto.text();
-    }
-
-    private String askToGpt(String assistantsId, String fullText) {
-        ThreadRunReqDto threadRunReqDto = ThreadRunReqDto.of(
-                assistantsId,
-                "user",
-                fullText
-        );
-
-        ThreadRunResDto threadRunResDto = openAiClient.threadRun(threadRunReqDto);
-
-        CountDownLatch latch = new CountDownLatch(1);
-
-        scheduler.scheduleAtFixedRate(() -> {
-            RunResDto runResDto = openAiClient.getRun(threadRunResDto.thread_id(), threadRunResDto.id());
-
-            if (runResDto.status().equals("completed")) {
-                latch.countDown();
-                scheduler.shutdown();
-            }
-        }, 0, 1, TimeUnit.SECONDS);
-
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-
-        }
-
-        MessagesResDto messagesResDto = openAiClient.threadMessages(threadRunResDto.thread_id());
-        return messagesResDto.getReply();
-    }
 }
