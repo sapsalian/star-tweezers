cd4312c6c7dd7f6ff63511bdd388da032f0fa472
Yongjin An <yongjin5352@gmail.com>
2024-08-13 14:02:14 +0900

    feat: Token에서 서비스 사용자 id 뽑아내는 리졸버 정리 컨트롤러에 적용

--- src/main/java/com/starforceps/starforceps/domain/organizer/api/OrganizerController.java
+++ src/main/java/com/starforceps/starforceps/domain/organizer/api/OrganizerController.java
@@ -3,6 +3,7 @@ package com.starforceps.starforceps.domain.organizer.api;
 import com.starforceps.starforceps.domain.organizer.application.OrganizerService;
 import com.starforceps.starforceps.domain.organizer.dto.OrganizerRequestDto;
 import com.starforceps.starforceps.domain.organizer.dto.OrganizerResponseDto;
+import com.starforceps.starforceps.global.common.custom_annotation.annotation.TokenId;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,20 +18,17 @@ public class OrganizerController {
     }
 
     @PostMapping(value = "/api/organizers", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public OrganizerResponseDto organize(@ModelAttribute OrganizerRequestDto organizerRequestDto) {
-        Long userId = (long)1;
+    public OrganizerResponseDto organize(@ModelAttribute OrganizerRequestDto organizerRequestDto, @TokenId Long userId) {
         return organizerService.organize(userId ,organizerRequestDto);
     }
 
     @GetMapping(value = "/api/organizers")
-    public List<OrganizerResponseDto> getOrganizers() {
-        Long userId = (long)1;
+    public List<OrganizerResponseDto> getOrganizers(@TokenId Long userId) {
         return organizerService.getOrganizers(userId);
     }
 
     @DeleteMapping(value = "/api/organizers/{organizer_id}")
-    public void deleteOrganizer(@PathVariable Long organizer_id) {
-        Long userId = (long)1;
+    public void deleteOrganizer(@PathVariable Long organizer_id, @TokenId Long userId) {
         organizerService.deleteOrganizer(userId, organizer_id);
     }
 }
